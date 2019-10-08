49eab4da69c6108527185dfcfea83cdee4ad27d6
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-08 17:51:15 +0900

    add participants to event forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,11 @@ class EventsController < ApplicationController
     @calendar = Calendar.find(params[:event][:calendar_id])
     @event = @calendar.events.build(event_params)
     if @event.save
-      @event.user_events.create(user_id: current_user.id)
+      @event.user_events.create(user_id: current_user.id, accepted: true)
+      params[:event][:participants].split(", ").each do |p|
+        participant = User.find_by(name: p)
+        UserEvent.create(user_id: participant.id, event_id: @event.id)
+      end
       flash[:success] = "イベントを追加しました"
       redirect_to root_path
     else
@@ -32,7 +36,14 @@ class EventsController < ApplicationController
   end
 
   def show
+    #FIXME: participantsが存在しない場合にバグ
     @event = Event.find(params[:id])
+    @participants = []
+    participants_id = UserEvent.where(event_id: @event.id, accepted: true).map(&:user_id)
+    participants_id.each do |p_id|
+      @participants << User.find_by(id: p_id)
+    end
+    @inviting = @event.users.where(accepted: false)
   end
 
   def update
@@ -51,9 +62,13 @@ class EventsController < ApplicationController
     redirect_to root_path
   end
 
+  def accept
+    current_user.user_events.find_by(event_id: params[:event_id]).update_attributes(accepted: true)
+  end
+
   private
 
   def event_params
-    params.require(:event).permit(:title, :description, :start_date, :end_date, :organizer_id)
+    params.require(:event).permit(:title, :description, :start_date, :end_date, :organizer_id, :calendar_id)
   end
 end
