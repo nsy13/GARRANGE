b56429ed7bc5989284dedb0e5af5cc9f9a8abe25
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-09 14:21:59 +0900

    add calendar_events table

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,5 @@
 class Calendar < ApplicationRecord
   belongs_to :user
-  has_many :events, dependent: :destroy
+  has_many :calendar_events, dependent: :destroy
+  has_many :events, through: :calendar_events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_071955) do
+ActiveRecord::Schema.define(version: 2019_10_09_051759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "calendar_events", force: :cascade do |t|
+    t.bigint "calendar_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["calendar_id"], name: "index_calendar_events_on_calendar_id"
+    t.index ["event_id"], name: "index_calendar_events_on_event_id"
+  end
 
   create_table "calendars", force: :cascade do |t|
     t.bigint "user_id"
@@ -32,8 +41,6 @@ ActiveRecord::Schema.define(version: 2019_10_08_071955) do
     t.datetime "updated_at", null: false
     t.integer "organizer_id"
     t.string "place"
-    t.bigint "calendar_id"
-    t.index ["calendar_id"], name: "index_events_on_calendar_id"
   end
 
   create_table "gcals", force: :cascade do |t|
@@ -75,8 +82,9 @@ ActiveRecord::Schema.define(version: 2019_10_08_071955) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "calendar_events", "calendars"
+  add_foreign_key "calendar_events", "events"
   add_foreign_key "calendars", "users"
-  add_foreign_key "events", "calendars"
   add_foreign_key "gcals", "users"
   add_foreign_key "sns_credentials", "users"
   add_foreign_key "user_events", "events"
