d2eb357fbaa7ae445601b6f92391a2ce5a771bf4
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-10 18:27:54 +0900

    delete unnecessary factory files

--- spec/factories/calendars_events.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :calendar_event do
-    sequence(:calendar_id) { 1 }
-    sequence(:event_id) { 1 }
-  end
-end
--- spec/factories/user_calendars.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user_calendar do
-    sequence(:user_id) { 1 }
-    sequence(:calendar_id) { 1 }
-    sequence(:owner) { false }
-  end
-end
