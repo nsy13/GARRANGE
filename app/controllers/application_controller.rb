219ba6090e6047ab4010bc369f17c3cbd0fc5f36
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-10 22:41:47 +0900

    modified authenticate_user redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     login_path
   end
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      flash[:info] = "ログインしてください"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @user = User.find_by(id: params[:user_id]) || current_user
