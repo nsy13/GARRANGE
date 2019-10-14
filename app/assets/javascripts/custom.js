8a3e5ed057fb69a3e8d35c938e051fdb653ff37a
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-15 01:58:54 +0900

    削除したイベントリスト用のJSを削除

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,16 +1,10 @@
 // events-indexの動的表示
 
 jQuery(document).ready(function() {
-  $('.events-index').children('div:gt(0)').hide();
-  $('.event-title').addClass('overflow-auto');
-
 	$('input[name="select-calendar"]').change(function() {
     // 初期化
     $('input[name="selected_calendars"]').remove();
-    $('.events-index').children('div').hide();
-
     // submitへのhidden要素追加
-    // カレンダー変更に伴う@eventsの動的な変更
     var selected_calendars = [];
     $('input[name="select-calendar"]:checked').each(function() {
       selected_calendars.push($(this).attr('id'));
@@ -21,17 +15,6 @@ jQuery(document).ready(function() {
       'value': selected_calendars
     }).appendTo($('.event-form, .select-calendar-form'));
     Rails.fire($('.select-calendar-form')[0], 'submit');
-
-    // events-indexの要素表示
-    if (selected_calendars.length === 0) {
-      $('.events-index p').show();
-    } else {
-      $('.events-index p').hide();
-      $.each(selected_calendars, function(index, value) {
-        $('.events-index').children('.' + value).show();
-        $('.event-title').addClass('overflow-auto');
-      });
-    };
 	});
 });
 
@@ -40,8 +23,8 @@ jQuery(document).ready(function() {
 $('.modal').on('shown.bs.modal', function() {
   $('.participateSubmit').click(function() {
     $('.attendance-form').submit();
-  })
-})
+  });
+});
 
 // カレンダー編集画面
 
