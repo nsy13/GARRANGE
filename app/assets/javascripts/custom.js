38e6273398a638b5810d285b8943ba9a126f0c53
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-10-16 23:43:08 +0900

    クラス名のBEM準拠とそれに伴う微修正

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,8 @@
-// events-indexの動的表示
-
+// Topページ
 jQuery(document).ready(function() {
+  // 選択したカレンダーに登録されたイベントを表示
 	$('input[name="select-calendar"]').change(function() {
-    // 初期化
     $('input[name="selected_calendars"]').remove();
-    // submitへのhidden要素追加
     var selected_calendars = [];
     $('input[name="select-calendar"]:checked').each(function() {
       selected_calendars.push($(this).attr('id'));
@@ -13,21 +11,13 @@ jQuery(document).ready(function() {
       'type': 'hidden',
       'name': 'selected_calendars',
       'value': selected_calendars
-    }).appendTo($('.event-form, .select-calendar-form'));
+    // イベント作成フォームのデフォルトカレンダーを表示しているカレンダーに設定
+    }).appendTo($('.sidebar__newEvent--form, .select-calendar-form'));
     Rails.fire($('.select-calendar-form')[0], 'submit');
 	});
 });
 
-// イベント参加モーダル
-
-$('.modal').on('shown.bs.modal', function() {
-  $('.participateSubmit').click(function() {
-    $('.attendance-form').submit();
-  });
-});
-
 // カレンダー編集画面
-
 $(document).ready(function() {
   $('#calendar_id').change(function() {
     var calendar_id = $('#calendar_id').val();
@@ -48,15 +38,20 @@ $(document).ajaxComplete(function() {
   });
 });
 
-// イベント作成フォームの参加者検索
+// モーダル関係
+$('.modal').on('shown.bs.modal', function() {
+  // 招待されたイベントへの参加
+  $('.participateSubmit').click(function() {
+    $('.attendance-form').submit();
+  });
 
-$('.modal').on('shown.bs.modal ajaxComplete', function() {
+  // イベント作成フォームの参加者検索
   $('.searched-users').children().hide();
   $('input[name="user_name_or_email"]').keyup(function() {
     $('.searched-users').children().hide();
     $('input[name="search_user"]:checked').parent().show();
     var user_info = $('input[name="user_name_or_email"]').val();
-    $("[class*='" + user_info + "']").show();
+    $('.searched-users').children($("[class*='" + user_info + "']")).show();
   });
   $('input[name="search_user"]').change(function(){
     $('input[name="inviting_users"]').remove();
@@ -68,19 +63,16 @@ $('.modal').on('shown.bs.modal ajaxComplete', function() {
       'type': 'hidden',
       'name': 'inviting_users',
       'value': inviting_users
-    }).appendTo($('#event-modal-form'));
+    }).appendTo($('.modal__newEvent--form, .modal__searchDate--form'));
   });
-  $('.event-form-submit').click(function() {
-    $('#event-modal-form').submit();
+  $('.modal__newEvent--submit').click(function() {
+    $('.modal__newEvent--submitDisplayNone').click();
   });
-});
 
-// 日程検索フォーム
-
-$('.modal').on('shown.bs.modal', function() {
-  $('.date-search-submit').click(function() {
+  // 日程検索フォーム
+  $('.modal__searchDate--submit').click(function() {
     $('span').click();
-    // Rails.fire($('#event-modal-form')[0], 'submit');
+    Rails.fire($('.modal__searchDate--form')[0], 'submit');
   });
 
   $('input[name="select_date"]').change(function() {
@@ -92,25 +84,5 @@ $('.modal').on('shown.bs.modal', function() {
       //   'name': 'selected-date',
       //   'value': selected_date
       // }).appendTo($('#event-modal-form'));
-    });
   });
-
-$(document).ajaxComplete(function() {
-  $('.modal').on('shown.bs.modal', function() {
-
-    $('.date-search-submit').click(function() {
-      Rails.fire($('#event-modal-form')[0], 'submit');
-    });
-
-    $('input[name="select_date"]').change(function() {
-      console.log('hello');
-      // $('input[name="selected-date]').remove();
-      // var selected_date = $('input[name="date_select"]:checked').val();
-      // $('<input>').attr({
-      //   'type': 'hidden',
-      //   'name': 'selected-date',
-      //   'value': selected_date
-      // }).appendTo($('#event-modal-form'));
-    });
-  })
 });
